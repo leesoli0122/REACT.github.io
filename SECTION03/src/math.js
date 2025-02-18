7680dfacba7d5aac81e12f8d6c95bdeec0cc5ac0
leesoli0122 <leesoli0122@gmail.com>
2025-02-18 13:14:36 +0900

    node.js 라이브러리 추가

--- SECTION03/src/math.js
+++ SECTION03/src/math.js
@@ -1,13 +1,13 @@
 // math 모듈
-function add(a,b) {
+export function add(a,b) {
     return a + b;
 }
 
-function sub(a,b) {
+export function sub(a,b) {
     return a - b;
 }
 
-module.export = {
-    add,
-    sub,
+export default function multiply(a,b){
+    return a * b;
 }
+
