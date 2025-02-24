cf08274799062325b3c37a310bd7cb0f03ff0c33
leesoli0122 <leesoli0122@gmail.com>
2025-02-24 17:20:53 +0900

    todolist end useReducer사용해서 업그레이드까지 끝

---
+++ SECTION08_09/src/components/TodoItem.jsx
@@ -0,0 +1,23 @@
+import './TodoItem.css'
+
+const TodoItem = ({id, isDone, content, date, onUpdate , onDelete}) => {
+
+    const onChangeCheckbox = () => {
+        onUpdate(id);
+    }
+
+    const onClickDeleteButton = () => {
+        onDelete(id);
+    }
+    return (
+        <div className="TodoItem">
+            {/* onClick가 아니라 onChange요소를 사용한 이유:버튼이 아니라 input이기 때문 */}
+            <input onChange={onChangeCheckbox} checked={isDone} type="checkbox" />
+            <div className="content">{content}</div>
+            <div className="date">{new Date(date).toLocaleDateString()}</div>
+            <button onClick={onClickDeleteButton}>삭제</button>
+        </div>
+    );
+}
+
+export default TodoItem;
