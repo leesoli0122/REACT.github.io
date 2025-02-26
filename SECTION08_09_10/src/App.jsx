f841847206e9f4b6007257f91c3dc0e27b543a11
leesoli0122 <leesoli0122@gmail.com>
2025-02-26 13:27:03 +0900

    최적화 end

--- SECTION08_09_10/src/App.jsx
+++ SECTION08_09_10/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState, useRef, useReducer } from 'react';
+import { useState, useRef, useReducer, useCallback } from 'react';
 import Header from './components/Header';
 import Editor from './components/Editor';
 import List from './components/List';
@@ -38,7 +38,7 @@ function App() {
   const [todos, dispatch] = useReducer(reducer, mockData);
   const idRef = useRef(3);
 
-  const onCreate = (content) => {
+  const onCreate = useCallback((content) => {
     dispatch({
       type : 'CREATE',
       data : {
@@ -48,23 +48,23 @@ function App() {
         date : new Date().getTime(),
       }
     })
-  };
+  }, []);
   
   // 추가
-  const onUpdate = (targetId) => {
+  const onUpdate = useCallback((targetId) => {
     dispatch({
       type : 'UPDATE',
       targetId : targetId,
     })
-  };
+  }, []);
   
   // 삭제버튼
-  const onDelete = (targetId) => {
+  const onDelete = useCallback((targetId) => {
     dispatch({
       type : 'DELETE',
       targetId : targetId,
-    })
-  };
+    });
+  }, []);
 
   return (
     <>
